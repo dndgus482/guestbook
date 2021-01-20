d12110c4ad9844d405f15bd8ebca9d150d71899e
dndgus482 <dndgus4820@gmail.com>
2021-01-20 21:29:09 +0900

    4.8 방명록의 조회 처리
    조회화면 만들기
    조회 기능 컨트롤러, 서비스 작성

--- src/main/java/com/dndgus482/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/dndgus482/guestbook/service/GuestbookServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -43,6 +44,13 @@ public class GuestbookServiceImpl implements GuestbookService {
         Function<Guestbook, GuestbookDTO> fn = (this::entityToDto);
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.map(this::entityToDto).orElse(null);
     }
 
 }
