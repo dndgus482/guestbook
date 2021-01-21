a264ae51d12e5df9f25a2bb0368f14d4fdfa79ca
dndgus482 <dndgus4820@gmail.com>
2021-01-21 11:30:37 +0900

    서비스 계층 검색기능 추가
    검색 키워드에 맞는 쿼리 만들기
    테스트 코드

--- src/test/java/com/dndgus482/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/com/dndgus482/guestbook/service/GuestbookServiceTests.java
@@ -46,4 +46,27 @@ public class GuestbookServiceTests {
         resultDTO.getPageList().forEach(System.out::println);
     }
 
+    @Test
+    public void testSearch() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(11)
+                .size(10)
+                .type("tc")
+                .keyword("1")
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("-----------------------------------");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("===================================");
+        resultDTO.getPageList().forEach(System.out::println);
+    }
 }
